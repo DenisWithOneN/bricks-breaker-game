bdf573f2b9efbd3b93dfbbed666672bfa1a05311
DenisWithOneN <denis.ursut@emanuel.com>
2023-03-17 09:30:52 +0200

    function setup & draw

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function setup() {
+
+    createCanvas(400, 400)
+}
+
+function draw() {
+    // 0,1,2,3 ... a,b,c,d,e,f HEXA
+    // RGB - ** ** **
+    // red - ff 00 00
+
+    background("#ff0000")
+}
