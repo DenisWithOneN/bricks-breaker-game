1f9bc31bfd8e1b3edab54bc852ffeb07234a4031
rebeca <rebeca.puscas@emanuel.ro>
2023-03-17 10:41:30 +0200

    smileyface-rebe

--- scripts.js
+++ scripts.js
@@ -8,5 +8,13 @@ function draw() {
     // RGB - ** ** **
     // red - ff 00 00
 
-    background("#ff0000")
+    background("#00ffff")
+
+    smileyFaceRebe(); 
+    
+}
+
+
+function smileyFaceRebe() {
+    circle(200, 200, 100); 
 }
