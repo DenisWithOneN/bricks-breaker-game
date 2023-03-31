d1358be0e84d1c4f4298fa7141eafa16d1b49aa7
rebeca <rebeca.puscas@emanuel.ro>
2023-03-31 11:01:30 +0300

    final

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-let moveS2X = 200;
-let moveS2Y = 200;
-let speedS2 = 5;
-rayS2 = 100; // 
+let moveS1X = 200;
+let moveS1Y = 200;
+let speedS1 = 5;
+rayS1 = 100; // 
 
-let sensS2X = 1;
-let sensS2Y = 1;
+let sensS1X = 1;
+let sensS1Y = 1;
 
 let limitTop = 0;
 let limitBottom = 400;
@@ -22,26 +22,26 @@ function draw() {
   
 
   //verificare limita Top S1
-  if (moveS2Y < limitTop + rayS2) {
-    sensS2Y = 1; //schimbare de sens
+  if (moveS1Y < limitTop + rayS1) {
+    sensS1Y = 1; //schimbare de sens
   }
   //verificare limmita Bottom S1
-  if (moveS2Y > limitBottom - rayS2) {
-    sensS2Y = -1; //schimbare de sens
+  if (moveS1Y > limitBottom - rayS1) {
+    sensS1Y = -1; //schimbare de sens
   }
   //verificare limmita Left S1
-  if (moveS2X < limitLeft + rayS2) {
-    sensS2X = 1; //schimbare de sens
+  if (moveS1X < limitLeft + rayS1) {
+    sensS1X = 1; //schimbare de sens
   }
   //verificare limmita Right S1
-  if (moveS2X > limitRight - rayS2) {
-    sensS2X = -1; //schimbare de sens
+  if (moveS1X > limitRight - rayS1) {
+    sens1X = -1; //schimbare de sens
   }
 
-  moveS2X += sensS2X * speedS2;
-  moveS2Y += sensS2Y * speedS2;
+  moveS1X += sensS1X * speedS1;
+  moveS1Y += sensS1Y * speedS1;
 
-  smileyFaceRebe(moveS2X, moveS2Y, rayS2 * 2);
+  smileyFaceRebe(moveS1X, moveS1Y, rayS1 * 2);
 }
 
 
@@ -58,6 +58,7 @@ function smileyFaceRebe(x, y, d) {
     circle(x+22, y-17, 7);
     circle(x-12, y-17, 7); 
        
+    
     //mouth
     fill("#cb7b97"); 
     arc(x, y+10, 40, 30, 0, Math.PI); 
