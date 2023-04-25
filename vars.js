1177f045a513b27b994615b369ff557a46b4311a
DenisWithOneN <denis.ursut@emanuel.com>
2023-04-25 10:31:38 +0300

    commit?

---
+++ vars.js
@@ -0,0 +1,25 @@
+// denis
+let moveS1X = 200;
+let moveS1Y = 300;
+
+let speedS1 = 5;
+rayS1 = 50;
+
+let sensS1X = 1;
+let sensS1Y = 1;
+// denis
+
+//rebe
+let moveS2X = 300;
+let moveS2Y = 400;
+let speedS2 = 3;
+rayS2 = 50;
+
+let sensS2X = 2;
+let sensS2Y = 2;
+//rebe
+
+let limitTop = 0;
+let limitBottom = 400;
+let limitLeft = 0;
+let limitRight = 400;
